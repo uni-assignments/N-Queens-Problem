7f64f6069b1e26f1a8e3da834c48b17b6a4916c5
ritarez <ritarezborges@gmail.com>
2021-06-16 22:13:31 -0300

    made code more concise

--- Functional/nqueens.hs
+++ Functional/nqueens.hs
@@ -1,10 +1,14 @@
 import System.Environment
 import Data.List
 
-nao_existe_conflito :: Int -> Int -> Int -> Bool
-nao_existe_conflito r1 r2 n 
-    | (r1 == r2) || ((r1 - r2) == n) || ((r1 - r2) == -n) = False
-    | otherwise = True
+posicao_valida :: Int -> [Int] -> Bool
+posicao_valida rainha_atual rainhas_no_tabuleiro = and [ nao_existe_conflito rainha rainha_atual n
+                        | (n,rainha) <- zip [1..] rainhas_no_tabuleiro]
+    where 
+        nao_existe_conflito :: Int -> Int -> Int -> Bool
+        nao_existe_conflito r1 r2 n 
+            | (r1 == r2) || (abs(r1 - r2) == n)  = False
+            | otherwise = True
 
 
 ndamas :: Int -> [[Int]]
@@ -15,10 +19,6 @@ ndamas n = solucao n []
         solucao n rainhas = concat [ solucao (n-1) (rainha:rainhas)
                             | rainha <- posicoes, posicao_valida rainha rainhas ]
         posicoes = [1..n]
-
-        posicao_valida :: Int -> [Int] -> Bool
-        posicao_valida rainha_atual rainhas_no_tabuleiro = and [ nao_existe_conflito rainha rainha_atual n
-                             | (n,rainha) <- zip [1..] rainhas_no_tabuleiro]
 
 main :: IO()
 main = do
